b1b275adbc9e4772390e4680e76d5b6cd32cb054
João Mannes <joao.mannes@rovitex.com.br>
2019-11-19 13:03:21 -0300

    First Commit

---
+++ RabbitMqConsole.Receiver/Program.cs
@@ -0,0 +1,65 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMqConsole.Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Receive();
+        }
+
+        static void Receive()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+
+                    if (ea.Redelivered)
+                    {
+                        Console.WriteLine("Redelivery Ack {0}", message);
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    }
+                    else
+                    {
+                        if (Int32.Parse(message) % 2 == 0)
+                        {
+                            Console.WriteLine("Ack {0}", message);
+                            channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Reject Ack");
+                            channel.BasicReject(deliveryTag: ea.DeliveryTag, true);
+                        }
+                    }
+                    
+                        
+                };
+                channel.BasicConsume(queue: "hello",
+                                     autoAck: false,
+                                     consumer: consumer);
+
+                Console.WriteLine("Aguardando Mensagens...");
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RabbitMqConsole/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMqConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Send();
+            
+        }
+
+        static void Send()
+        {
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                while (true)
+                {
+                    for (int i = 0; i < 20; i++)
+                    {
+                        string message = i.ToString();
+
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish(exchange: "",
+                                             routingKey: "hello",
+                                             basicProperties: null,
+                                             body: body);
+                        Console.WriteLine(" [x] Sent {0}", message);
+                    }
+                    break;
+                }
+                
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
